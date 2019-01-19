a3b82e8d40a4017c78053cb18ad7b8d0999bdf95
phuduc <phuduc212@gmail.com>
2019-01-19 23:10:48 +0700

    Adding lowdb and user view

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 // import express from 'express';
 const express = require('express');
+const bodyParser = require('body-parser');
+const low = require('lowdb');
+const shortid = require('shortid');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+db.defaults({ users: [] })
+  .write()
 
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', (req, res) => {
     res.render('index', {
@@ -15,12 +28,37 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     res.render('users/index', {
-        users: [
-            { id: 1, name: 'Duc'},
-            { id: 2, name: 'Long'},
-            { id: 3, name: 'Vi'}
-        ]
+        users: db.get('users').value()
     });
+})
+
+app.get('/users/search', (req, res) => {
+    const q = req.query.q;
+    const matchedUsers = users.filter((user) => {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+    
+    res.render('users/index', {
+        users: matchedUsers
+    });
+});
+
+app.get('/users/create', (req, res) => {
+    res.render('users/create');
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = db.get('users').find({ id: id}).value();
+    res.render('users/view', {
+        user: user
+    });
+});
+
+app.post('/users/create', (req, res) => {
+    req.body.id = shortid.generate();
+    db.get('users').push(req.body).write();
+    res.redirect('/users');
 })
 
 app.listen(port, () => {
