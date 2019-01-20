53b1b6e32250841f7e9be7a9118e4961dcd4a097
phuduc <phuduc212@gmail.com>
2019-01-20 14:49:58 +0700

    Static files and User validation

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.set('views', './views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.render('index', {
         name: 'AAA'
